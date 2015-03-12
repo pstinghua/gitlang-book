4e7a503e6957fcd8490d31de724895abdfe6de9b
pstinghua <pstinghua@gmail.com>
2015-03-13 05:12:28 +0800

    updated

---
+++ chapter10/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sleep(s int) {
+	<-time.After(time.Second * time.Duration(s))
+}
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "from 1"
+			// time.Sleep(time.Second * 2)
+			sleep(2)
+		}
+	}()
+	go func() {
+		for {
+			c2 <- "from 2"
+			// time.Sleep(time.Second * 3)
+			sleep(10)
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			case <-time.After(time.Second):
+				fmt.Println("timeout")
+				// default:
+				// 	fmt.Println("nothing ready")
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scanln(&input)
+}
--- chapter9/main.go
+++ chapter9/main.go
@@ -5,7 +5,36 @@ import (
 	"math"
 )
 
-func distance(x1, y1, x2, y2 float64) float
+type Shape interface {
+	area() float64
+	perimeter() float64
+}
+
+type MultiShape struct {
+	shapes []Shape
+}
+
+func (m *MultiShape) area() float64 {
+	var area float64
+	for _, s := range m.shapes {
+		area += s.area()
+	}
+	return area
+}
+
+func totalArea(shapes ...Shape) float64 {
+	var area float64
+	for _, s := range shapes {
+		area += s.area()
+	}
+	return area
+}
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
 
 type Circle struct {
 	x, y, r float64
@@ -15,16 +44,49 @@ func (c *Circle) area() float64 {
 	return math.Pi * c.r * c.r
 }
 
+func (c *Circle) perimeter() float64 {
+	return 2 * math.Pi * c.r
+}
+
 type Rectangle struct {
 	x1, y1, x2, y2 float64
 }
 
 func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
 
+func (r *Rectangle) perimeter() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return 2 * l * w
+}
+
+type Person struct {
+	Name string
+}
+
+func (p *Person) Talk() {
+	fmt.Println("Hi, my name is", p.Name)
+}
+
+type Android struct {
+	Person
+	Model string
 }
 
 func main() {
 	c := Circle{0, 0, 5}
-	// fmt.Println(circleArea(&c))
 	fmt.Println(c.area())
+
+	r := Rectangle{0, 0, 10, 10}
+	fmt.Println(r.area())
+
+	fmt.Println(totalArea(&c, &r))
+
+	a := new(Android)
+	a.Person.Talk()
+	a.Talk()
 }
